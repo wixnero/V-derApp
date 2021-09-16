cef5d14992b76e6d44fb7ea0c2083afee780f487
wixnero <plumpen_96@hotmail.com>
2021-09-16 23:41:58 +0200

    navbar added, overlooked colors

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-
-const currentLocation = {long:"", lat:"", temp:0, city:""};
+const currentLocation = { long: "", lat: "", temp: 0, city: "" };
 GetGeoLocation();
-function GetGeoLocation()
-{
-    navigator.geolocation.getCurrentPosition(setLongLangAPI)
+function GetGeoLocation() {
+  navigator.geolocation.getCurrentPosition(setLongLangAPI);
 }
 
-function setLongLangAPI(pos)
-{
+function setLongLangAPI(pos) {
+  currentLocation.long = pos.coords.longitude;
+  currentLocation.lat = pos.coords.latitude;
+  console.log(currentLocation.long);
+  console.log(currentLocation.lat);
+  var APIstring =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    currentLocation.lat +
+    "&lon=" +
+    currentLocation.long +
+    "&units=metric&appid=b5a6a4e37d967936d274f4adc367b506";
 
-    currentLocation.long = pos.coords.longitude;
-    currentLocation.lat = pos.coords.latitude;
-    console.log(currentLocation.long);
-    console.log(currentLocation.lat);
-    var APIstring = 'https://api.openweathermap.org/data/2.5/weather?lat='+ currentLocation.lat +'&lon='+ currentLocation.long +'&units=metric&appid=b5a6a4e37d967936d274f4adc367b506';
-
-    SetCurrentLocation(APIstring);
+  SetCurrentLocation(APIstring);
 }
 
-function SetCurrentLocation(APIstring)
-{
-    console.log(APIstring);
-    try {
-        fetch(APIstring)
-       .then(function(response){
+function SetCurrentLocation(APIstring) {
+  console.log(APIstring);
+  try {
+    fetch(APIstring)
+      .then(function (response) {
         return response.json();
-         })
-        .then(function(obj){
+      })
+      .then(function (obj) {
         currentLocation.city = obj.name;
         currentLocation.temp = obj.main.feels_like;
-        document.getElementById("current_temp").innerHTML = currentLocation.temp + "°C";
-        document.getElementById("current_zone_name").innerHTML = currentLocation.city;
+        document.getElementById("current_temp").innerHTML =
+          currentLocation.temp + "°C";
+        document.getElementById("current_zone_name").innerHTML =
+          currentLocation.city;
         console.log(obj.main.feels_like);
-    });      
-    } catch (error) {
-        console.log(error);
-    }
-    
+      });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
-        
-
